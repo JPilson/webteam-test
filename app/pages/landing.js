82df34d7bd826c8e8793332c839e187c8c3bec05
Joao Sumbo (J.P) <43365113+JPilson@users.noreply.github.com>
2023-03-07 20:16:11 +0100

    Added Data

--- app/pages/landing.js
+++ app/pages/landing.js
@@ -14,26 +14,27 @@ import dotmemory from "@jetbrains/logos/dotmemory/dotmemory.svg";
 import dottrace from "@jetbrains/logos/dottrace/dottrace.svg";
 
 
-const logosGrid = document.querySelector(".ides-logos-grid")
+const idesListContainer = document.querySelector(".ides-list")
+const extensionPluginsContainer = document.querySelector(".extension-plugins")
 const idesList = [
-  {name: "intellijIdeaCe", logo: intellijIdeaCe},
-  {name: "webstorm", logo: webstorm},
-  {name: "resharper", logo: resharper},
-  {name: "rider", logo: rider},
-  {name: "appcode", logo: appcode},
-  {name: "clion", logo: clion},
-  {name: "rubymine", logo: rubymine},
-  {name: "datagrip", logo: datagrip},
-  {name: "phpstorm", logo: phpstorm},
-  {name: "pycharm", logo: pycharm},
+  {name: "intellijIdeaCe", logo: intellijIdeaCe,label:"IntelliJ IDEA Ultimate",tags:["Kotlin","Java"]},
+  {name: "webstorm", logo: webstorm,label:"WebStorm",tags:["Javascript","Typescript"]},
+  {name: "resharper", logo: resharper,label:"ReSharper Ultimate",tags:["C++"]},
+  {name: "rider", logo: rider,label:"Rider",tags:["C#",".NET"]},
+  {name: "appcode", logo: appcode,label:"AppCode",tags:["IOS","SWIFT"]},
+  {name: "clion", logo: clion,label:"CLion",tags:["C","C++"]},
+  {name: "rubymine", logo: rubymine,label:"RubyMine",tags:["Ruby","Rails"]},
+  {name: "datagrip", logo: datagrip,label:"DataGrip",tags:["mysql"]},
+  {name: "phpstorm", logo: phpstorm,label:"PhpStorm",tags:["Php","Database"]},
+  {name: "pycharm", logo: pycharm,label:"PyCharm",tags:["Python"]},
 
 ]
-const extentionAndProfilers = [
-  {name: "resharper", logo: resharper},
-  {name: "resharperCpp", logo: resharperCpp},
-  {name: "dotcover", logo: dotcover},
-  {name: "dotmemory", logo: dotmemory},
-  {name: "dottrace", logo: dottrace}
+const extensionAndProfilers = [
+  {name: "resharper", logo: resharper,label:"ReSharper Ultimate",tags:["C++"]},
+  {name: "resharperCpp", logo: resharperCpp,label:"ReSharper C++",tags:["C++"]},
+  {name: "dotcover", logo: dotcover,label:"dotCover",tags:[".Net","Test"]},
+  {name: "dotmemory", logo: dotmemory,label:"dotMemory",tags:[".Net","Test"]},
+  {name: "dottrace", logo: dottrace,label:"dotTrace",tags:[".Net","Test"]}
 
 ]
 
@@ -42,5 +43,12 @@ idesList.forEach(it => {
   const img = document.createElement("img")
   img.src = it.logo
   img.alt = it.name
-  logosGrid.appendChild(img)
+  idesListContainer.appendChild(img)
+})
+
+extensionAndProfilers.forEach(it=>{
+  const img = document.createElement("img")
+  img.src = it.logo
+  img.alt = it.name
+  extensionPluginsContainer.appendChild(img)
 })
